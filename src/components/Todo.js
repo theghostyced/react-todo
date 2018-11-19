4970a25cb4faa29b74eceb07cf9bb0456db664b5
okoroemeka <solomonokoro056@gmail.com>
2018-11-19 14:49:59 +0100

    create parent component for todo

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Todo extends React.Component{
+
+    render(){
+        return (
+        <div>
+            <div className='todo'>
+
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Todo;
